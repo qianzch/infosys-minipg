3125f0b66d811e211de81f175781595853b03042
Sanctimony <qym925998054@qq.com>
2018-05-18 09:27:46 +0800

    开始界面

---
+++ client/pages/Home/Home.js
@@ -0,0 +1,25 @@
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    console.log("Home OnLoad");
+  },
+  onReady: function () {
+    // 页面渲染完成
+    console.log("Home OnReady");
+  },
+  onShow: function () {
+    // 页面显示
+    console.log("Home OnShow");
+  },
+  onHide: function () {
+    // 页面隐藏
+    console.log("Home OnHide");
+  },
+  onUnload: function () {
+    // 页面关闭
+    console.log("Home OnUnload");
+  }
+})
---
+++ client/pages/Sign In/Sign In.js
@@ -0,0 +1,25 @@
+Page({
+  data: {
+    
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    console.log("Sign In OnLoad");
+  },
+  onReady: function () {
+    // 页面渲染完成
+    console.log("Sign In OnReady");
+  },
+  onShow: function () {
+    // 页面显示
+    console.log("Sign In OnShow");
+  },
+  onHide: function () {
+    // 页面隐藏
+    console.log("Sign In OnHide");
+  },
+  onUnload: function () {
+    // 页面关闭
+    console.log("Sign In OnUnload");
+  }
+})
