f4e9e913b620e2cbf2914290b0ee98ffb142e9e2
Ericccccie <qianzch@qq.com>
2018-05-18 09:06:04 +0800

    Added select function
    select info by ids/labels/keywords

--- server/database.py
+++ server/database.py
@@ -3,16 +3,24 @@ import MySQLdb
 from log import LOG
 
 class ERR():
-	SUCCESS 		= 0
-	ERR_UNKNOWN 	= 1
-	USR_EXISTED 	= 11
-	USR_NOT_EXISTS	= 12
+	SUCCESS 			= 0
+	ERR_UNKNOWN 		= 1
+	USR_EXISTED 		= 11
+	USR_NOT_EXISTS		= 12
+	ALREADY_LIKED 		= 13
+	LIKES_NOT_EXIST 	= 14
+	SOME_ID_NOT_EXISTS	= 15
+	EMPTY_SELECT_REQ 	= 16
 
 	STR = {
 		0:	'SUCCESS',
 		1:	'ERR_UNKNOWN',
 		11:	'USR_EXISTED',
-		12: 'USR_NOT_EXISTS'
+		12:	'USR_NOT_EXISTS',
+		13:	'ALREADY_LIKED',
+		14:	'LIKES_NOT_EXIST',
+		15:	'SOME_ID_NOT_EXISTS',
+		16:	'EMPTY_SELECT_REQ'
 	}
 
 class USR():
@@ -65,6 +73,9 @@ class INFO():
 
 	def construct(self, sql_obj):
 		pass
+		
+	def construct_from_sql(self, sql_obj):
+		pass
 
 # config
 HOST		= '123.207.11.16'
@@ -93,6 +104,7 @@ class DB():
 			self.db.close()
 			self.db = None
 
+	# register
 	def add_usr(self, usr):
 		status = ERR.SUCCESS
 		self.__connect()
@@ -111,7 +123,7 @@ class DB():
 		self.__close()
 		return status
 
-	# used for login
+	# login
 	def get_usr_by_name(self, usr_name):
 		status = ERR.SUCCESS
 		self.__connect()
@@ -128,11 +140,150 @@ class DB():
 			else:
 				status = ERR.USR_NOT_EXISTS
 		except Exception as e:
-			self.db.rollback()
 			status = ERR.ERR_UNKNOWN
 			LOG.loge(str(e))
 		self.__close()
 		return status, usr
+	
+	# insert like
+	def add_usr_like(self, usr_name, info_id):
+		status = ERR.SUCCESS
+		self.__connect()
+		cmd = """
+		insert into usr_like values(
+			'{0}', '{1}'
+		)
+		""".format(usr_name, info_id)
+		try:
+			self.cursor.execute(cmd)
+			self.db.commit()
+		except Exception as e:
+			self.db.rollback()
+			status = ERR.ALREADY_LIKED
+			LOG.loge(str(e))
+		self.__close()
+		return status
+	
+	# get likes
+	def get_usr_likes_by_name(self, usr_name):
+		status = ERR.SUCCESS
+		self.__connect()
+		cmd = """
+		select * from usr_like
+		where usr_name = '{}'
+		""".format(usr_name)
+		likes = []
+		try:
+			self.cursor.execute(cmd)
+			results = self.cursor.fetchall()
+			if results:
+				for e in results:
+					likes.append(e[1])
+			else:
+				status = ERR.LIKES_NOT_EXIST
+		except Exception as e:
+			status = ERR.ERR_UNKNOWN
+			LOG.loge(str(e))
+		self.__close()
+		return status, likes
+		
+	# remove like
+	def remove_usr_like(self, usr_name, info_id):
+		status = ERR.SUCCESS
+		self.__connect()
+		cmd = """
+		delete from usr_like
+		where usr_name = '{0}' and info_id = '{1}'
+		""".format(usr_name, info_id)
+		try:
+			self.cursor.execute(cmd)
+			db.commit()
+		except Exception as e:
+			self.db.rollback()
+			status = ERR.LIKES_NOT_EXIST
+			LOG.loge(str(e))
+		self.__close()
+		return status
+	
+	def __get_info_by_ids(self, ids):
+		status = ERR.SUCCESS
+		self.__connect()
+		infos = []
+		for e in ids:
+			cmd = """
+			select * from info
+			where info_id = '{}'
+			""".format(e)
+			try:
+				self.cursor.execute(cmd)
+				results = self.cursor.fetchall()
+				if results:
+					info = INFO()
+					info.construct_from_sql(results[0])
+					infos.append(info)
+				else:
+					status = ERR.SOME_ID_NOT_EXISTS
+			except Exception as e:
+				status = ERR.ERR_UNKNOWN
+				LOG.loge(str(e))
+		self.__close()
+		return status, infos
+		
+	def __get_info_by_labels(self, labels):
+		status = ERR.SUCCESS
+		self.__connect()
+		infos = []
+		for e in labels:
+			cmd = """
+			select * from info
+			where info_id = '{}'
+			""".format(e)
+			try:
+				self.cursor.execute(cmd)
+				results = self.cursor.fetchall()
+				if results:
+					info = INFO()
+					info.construct_from_sql(results[0])
+					infos.append(info)
+				else:
+					status = ERR.SOME_ID_NOT_EXISTS
+			except Exception as e:
+				status = ERR.ERR_UNKNOWN
+				LOG.loge(str(e))
+		self.__close()
+		return status, infos
+		
+	def __get_info_by_keywds(self, keywds):
+		return ERR.ERR_UNKNOWN, []
+
+	def __get_info_by_labels_keywds(self, labels, keywds):
+		status = ERR.SUCCESS
+		self.__connect()
+		infos = []
+		keywords = keywds 
+		return ERR.ERR_UNKNOWN, []
+
+	# get info
+	def get_info_by_req(self, req_select):
+		# see 'receive.py' for more info about 'req_select'
+		status = ERR.EMPTY_SELECT_REQ
+		infos = []
+		keywds = req_select.keywords.strip() # split here
+		# if ids are given, then select info use ONLY ids
+		if len(req_select.ids) > 0:
+			return self.__get_info_by_ids(req_select.ids)
+		if len(req_select.labels) > 0:
+			status, infos = self.__get_info_by_labels(req_select.labels)
+			if not status == ERR.SUCCESS:
+				return status, infos
+		if not keywds == '':
+			if len(req_select.labels) > 0:
+				return self.__get_info_by_labels_keywds(req_select.labels, keywds)
+			else:
+				return self.__get_info_by_keywds(keywds)
+		return status, infos
+
+
 
 
 db = DB()
