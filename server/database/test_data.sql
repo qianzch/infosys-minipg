a38a04c7244728977ef83f75e9a82a84ff41da1f
Ericccccie <qianzch@qq.com>
2018-05-22 08:42:55 +0800

    Revert "Revert "Finished select by ids/labels""
    This reverts commit ef0f2085f5a9fedca90d9f8a244b81d8c6971a09.

---
+++ server/database/test_data.sql
@@ -0,0 +1,23 @@
+use infosys_minipg;
+insert into info values(
+	null, 'dev', '6000', '10', 'developer of c++', 'C++', '', 
+    'Netease', 'A Pig-Breeding Company', '',
+    '2018-05-18 09:16:00', '15621110000', 'hr@163.com',
+    '2018-05-18', '10'
+);
+insert into info values(
+	null, 'dev', '5000', '1', 'developer of python', 'Py', '', 
+    'Netease', 'A Pig-Breeding Company', '',
+    '2018-05-18 09:16:00', '15621110000', 'hr@163.com',
+    '2018-05-18', '10'
+);
+
+insert into info_label values(
+	1, 'C++'
+);
+insert into info_label values(
+	1, 'dev'
+);
+insert into info_label values(
+	2, 'Python'
+);
